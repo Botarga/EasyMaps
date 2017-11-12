204d7e8b4afac416762345fcabc1eee9ea481ef3
botarga <mariovivasdavid@hotmail.es>
2017-11-12 11:17:47 +0100

    Conseguido visualizar camara preview

--- EasyMaps/app/src/main/java/com/hackaton/bordarga/easymaps/MainActivity.java
+++ EasyMaps/app/src/main/java/com/hackaton/bordarga/easymaps/MainActivity.java
@@ -1,13 +1,43 @@
 package com.hackaton.bordarga.easymaps;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Camera mCamera;
+    private CameraPreview mPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(checkCameraHardware(getApplicationContext())){
+            mCamera = getCameraInstance();
+            mPreview = new CameraPreview(this, mCamera);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
+        }
+
+    }
+
+    private boolean checkCameraHardware(Context context){
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try{
+            c = Camera.open();
+        }catch(Exception e){
+            String error = e.getMessage();
+        }
+
+        return c;
     }
 }
