b8ae9a7a22a23a8e1ce0f21004c1ca2945e9dec1
botarga <mariovivasdavid@hotmail.es>
2017-11-12 09:50:50 +0100

    Commit inicial

---
+++ EasyMaps/app/src/main/java/com/hackaton/bordarga/easymaps/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hackaton.bordarga.easymaps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
